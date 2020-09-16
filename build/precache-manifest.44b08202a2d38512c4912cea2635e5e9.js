2bad2097a930616fa350fe4cf166f88a0df3e671
Kovacs Bela Richard <richy199898@gmail.com>
2020-09-16 14:38:17 +0300

    something

---
+++ build/precache-manifest.44b08202a2d38512c4912cea2635e5e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b3a8f5a4f98de8bcab881e015bdaba",
+    "url": "/tvshows/index.html"
+  },
+  {
+    "revision": "40c05f2dc6142df466f2",
+    "url": "/tvshows/static/css/main.84487dca.chunk.css"
+  },
+  {
+    "revision": "2b327254153af6e2e108",
+    "url": "/tvshows/static/js/2.22159b1d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tvshows/static/js/2.22159b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c05f2dc6142df466f2",
+    "url": "/tvshows/static/js/main.5c442286.chunk.js"
+  },
+  {
+    "revision": "c5bb5f4dad86385eefaf",
+    "url": "/tvshows/static/js/runtime-main.90897355.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tvshows/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,6 +4,10 @@ import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import './styles/Main.css'
 
+
+// api key a04e46bcb6a28479586d4331d7049f7f
+// api url https://api.themoviedb.org/3/movie/550?api_key=a04e46bcb6a28479586d4331d7049f7f
+
 class MainPage extends React.Component {
     
     constructor(){
@@ -11,6 +15,8 @@ class MainPage extends React.Component {
         this.state = {
             wasSearched : false,
             inputValue: '',
+            searchResult: [],
+            isLoaded: false,
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -23,7 +29,33 @@ class MainPage extends React.Component {
 
     
     handleSearch() {
-        this.setState({wasSearched: !this.state.wasSearched})
+        if(!this.state.wasSearched)
+            this.setState({wasSearched: !this.state.wasSearched});
+        const path =  "https://api.trakt.tv?query="
+                        + this.state.inputValue.replace(" ", "+");
+        console.log(path);
+        fetch(path, {
+            method: 'get', 
+            headers: new Headers({ 
+              'Content-Type': 'application/json',
+              'traky-api-key':'981bac103e7fd2e491930d27794331a414eba41e14026a392c24b149d6f554ef',
+              'trakt-api-version': '2',
+            }),  
+        })
+        .then(res => res.json())
+        .then(
+          (result) => {
+              console.log(result);
+            this.setState({
+              isLoaded: true,
+              searchResult: result.tv_shows
+            });
+        },
+        (error) => {
+            console.log(error);
+        }
+      );
+      console.log(this.state.searchResult);
     }
     
     render() {
