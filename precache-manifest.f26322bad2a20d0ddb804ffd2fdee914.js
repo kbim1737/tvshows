c034320859d732b47866d9698a82c2dc17cfff5f
Kovacs Bela Richard <richy199898@gmail.com>
2020-09-16 12:20:49 +0300

    Updates

---
+++ precache-manifest.f26322bad2a20d0ddb804ffd2fdee914.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ead08f18f641b2b51c94a1b2c60934d",
+    "url": "/tvshows/index.html"
+  },
+  {
+    "revision": "f2f445fce36a01b92984",
+    "url": "/tvshows/static/css/main.84487dca.chunk.css"
+  },
+  {
+    "revision": "2b327254153af6e2e108",
+    "url": "/tvshows/static/js/2.22159b1d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tvshows/static/js/2.22159b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f445fce36a01b92984",
+    "url": "/tvshows/static/js/main.96382df8.chunk.js"
+  },
+  {
+    "revision": "c5bb5f4dad86385eefaf",
+    "url": "/tvshows/static/js/runtime-main.90897355.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tvshows/static/media/logo.5d5d9eef.svg"
+  }
+]);
